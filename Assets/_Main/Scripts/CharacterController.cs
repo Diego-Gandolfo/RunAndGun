1e38bab8f2891622a90541e5b1f831ecf4a5a6b3
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-01-15 16:39:41 -0300

    Salto, Tags, Layers y Camara

--- Assets/_Main/Scripts/CharacterController.cs
+++ Assets/_Main/Scripts/CharacterController.cs
@@ -10,9 +10,13 @@ namespace Character
     {
         [Header("Movement")]
         [SerializeField] private float moveVelocity = 0;
-
         private float moveX = 0;
         private float moveY = 0;
+
+        [Header("Jump")]
+        [SerializeField] private float jumpImpulse = 0;
+        [SerializeField] private float moveReduction = 0;
+        private bool canJump = false;
 
         private Rigidbody2D rb = null;
 
@@ -23,16 +27,39 @@ namespace Character
             rb = GetComponent<Rigidbody2D>();
         }
 
+        private void Start()
+        {
+            canJump = true;
+        }
+
         private void Update()
         {
             moveX = Input.GetAxis("Horizontal") * moveVelocity;
 
             if (Input.GetAxis("Fire1") > 0) OnFire?.Invoke();
+
+            if (Input.GetKeyDown(KeyCode.Space) && canJump) DoJump();
         }
 
         private void FixedUpdate()
         {
-            rb.velocity = new Vector2(moveX, moveY);
+            rb.velocity = new Vector2(moveX, rb.velocity.y);
+        }
+
+        private void DoJump()
+        {
+            canJump = false;
+            rb.AddForce(transform.up * jumpImpulse, ForceMode2D.Impulse);
+            moveVelocity -= moveReduction;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Floor"))
+            {
+                moveVelocity += moveReduction;
+                canJump = true;
+            }
         }
     }
 }
